ecf4ca5fcb65982207bfa5ab4fba0dd9dd0c3f12
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-07-15 15:09:56 -0400

    Agragando Entidad Compras

---
+++ Entidades/Compras.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        public int ProveedorId { get; set; }
+        public int UsuarioId { get; set; }
+        public double Total { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Compras()
+        {
+            CompraId = 0;
+            ProveedorId = 0;
+            UsuarioId = 0;
+            Total = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}

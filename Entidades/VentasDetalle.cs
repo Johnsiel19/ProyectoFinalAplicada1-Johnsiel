b398ccc49118709c3f788c9a147d7bc44c16fb12
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-07-15 15:14:39 -0400

    Agrgando entidad ventasDetalle

---
+++ Entidades/VentasDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class VentasDetalle
+    {
+        [Key]
+        public int VentaDetalleId { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }
+
+        public VentasDetalle()
+        {
+            VentaDetalleId = 0;
+            VentaId = 0;
+            ProductoId = 0;
+            Cantidad = 0;
+            Precio = 0;
+        }
+    }
+}

739a23dbd2fa46eaff9052389a2d97d8e9c6aae6
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-07-15 14:37:56 -0400

    Agregando Entidad producto

---
+++ Entidades/Productos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int ProveedorId { get; set; }
+        public int UsuarioId { get; set; }
+        public double Existencia { get; set; }
+        public double Costo { get; set; }
+        public double Precio { get; set; }
+        public int ProductoITBIS { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            ProveedorId = 0;
+            UsuarioId = 0;
+            Existencia = 0;
+            Costo = 0;
+            Precio = 0;
+            ProductoITBIS = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}

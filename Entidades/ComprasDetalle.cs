f3a5f1af3d66fbf2747fba4c080c17319155dd06
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-07-15 15:18:17 -0400

    Agragando Entidad Compras detalle

---
+++ Entidades/ComprasDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class ComprasDetalle
+    {
+        [Key]
+        public int CompraDetalleId { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public double Costo { get; set; }
+
+        public ComprasDetalle()
+        {
+            CompraDetalleId = 0;
+            CompraId = 0;
+            ProductoId = 0;
+            Cantidad = 0;
+            Costo = 0;
+        }
+    }
+}

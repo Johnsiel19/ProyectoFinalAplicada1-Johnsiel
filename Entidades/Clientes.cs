a3cbe819bf4ad8e361385e89a4da4cba5e821854
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-07-15 14:23:15 -0400

    Agregando Entidad Cliente

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombre = string.Empty;
+            Cedula = string.Empty;
+            Email = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Direccion = string.Empty;
+            UsuarioId = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}
